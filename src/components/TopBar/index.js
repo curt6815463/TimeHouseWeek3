11199b276814984fe5668cb08eb9934af7c54987
curt <curt6815463@gmail.com>
2018-08-13 15:31:46 +0800

    fix: webpack @ sign setting

---
+++ src/components/TopBar/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class TopBar extends Component {
+  render() {
+    return (
+      <div>
+        aaa
+      </div>
+    );
+  }
+}
+
+export default TopBar;
